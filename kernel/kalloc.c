0a640d08138d8e2237dbb79bbdf7fd89c54aa539
ivan <1185093533@qq.com>
2022-04-29 18:08:54 +0800

    i== cpu放在循環里會出錯……

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -21,14 +21,21 @@ struct run {
 struct {
   struct spinlock lock;
   struct run *freelist;
-} kmem;
+} kmem[NCPU];
+
+uint64 getcpuId(){
+  return cpuid();
+}
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
+  for (int i = 0;i < NCPU;i++){
+      initlock(&kmem[i].lock, "kmem");
+  }
   freerange(end, (void*)PHYSTOP);
 }
+
 
 void
 freerange(void *pa_start, void *pa_end)
@@ -46,6 +53,7 @@ freerange(void *pa_start, void *pa_end)
 void
 kfree(void *pa)
 {
+
   struct run *r;
 
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
@@ -56,27 +64,102 @@ kfree(void *pa)
 
   r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+  push_off();
+  uint64 cpu = cpuid();
+
+  acquire(&kmem[cpu].lock);
+  r->next = kmem[cpu].freelist;
+  kmem[cpu].freelist = r;
+  release(&kmem[cpu].lock);
+  pop_off();
 }
 
 // Allocate one 4096-byte page of physical memory.
 // Returns a pointer that the kernel can use.
 // Returns 0 if the memory cannot be allocated.
+
+void* kallocFromIdx(uint64 cpuid){
+ struct run *r=0;
+  acquire(&kmem[cpuid].lock);
+  r = kmem[cpuid].freelist;
+  if(r){
+    kmem[cpuid].freelist = r->next;
+  }
+  release(&kmem[cpuid].lock);
+  return (void*)r;
+
+}
+
 void *
 kalloc(void)
 {
   struct run *r;
+  push_off();
+  int cpu = cpuid();
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
-  if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+  acquire(&kmem[cpu].lock);
+ if (!kmem[cpu].freelist){
 
+    for (int i = 0;i < NCPU&&!kmem[cpu].freelist;i++){
+    if (i == cpu) continue; //dangshi xiezai xunhuan li chucuole ...
+//      struct run* p = kallocFromIdx(i);
+//      if (p!=0){
+//        p->next = kmem[cpuid].freelist;
+//                kmem[cpuid].freelist = p;
+//        break;
+//      }
+      acquire(&kmem[i].lock);
+      struct run *rr = kmem[i].freelist;
+      if (rr) {
+        kmem[i].freelist = rr->next;
+        rr->next = kmem[cpu].freelist;
+        kmem[cpu].freelist = rr;
+      }
+      release(&kmem[i].lock);
+      }
+  }
+   r = kmem[cpu].freelist;
+
+  if(r){
+       kmem[cpu].freelist = r->next;
+
+     }
+    release(&kmem[cpu].lock);
+  pop_off();
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
   return (void*)r;
+// struct run *r;
+//
+//  push_off();
+//
+//  int cpu = cpuid();
+//
+//  acquire(&kmem[cpu].lock);
+//
+//  if(!kmem[cpu].freelist) { // no page left for this cpu
+//    //int steal_left = 64; // steal 64 pages from other cpu(s)
+//    for(int i=0;i<NCPU&&!kmem[cpu].freelist;i++) {
+//      if(i == cpu) continue; // no self-robbery
+//      acquire(&kmem[i].lock);
+//      struct run *rr = kmem[i].freelist;
+//      if (rr) {
+//        kmem[i].freelist = rr->next;
+//        rr->next = kmem[cpu].freelist;
+//        kmem[cpu].freelist = rr;
+//      }
+//      release(&kmem[i].lock);
+//    }
+//  }
+//
+//  r = kmem[cpu].freelist;
+//  if(r)
+//    kmem[cpu].freelist = r->next;
+//  release(&kmem[cpu].lock);
+//
+//  pop_off();
+//
+//  if(r)
+//    memset((char*)r, 5, PGSIZE); // fill with junk
+//  return (void*)r;
 }
